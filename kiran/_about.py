e56db24e98542ceac2237fd74d779a89d3ec5b2d
unknown <halfstackpgr@gmail.com>
2024-07-28 22:48:14 +0530

    Changes in banner, and details that are put up in banner.

--- kiran/_about.py
+++ kiran/_about.py
@@ -2,29 +2,29 @@
 
 from __future__ import annotations
 
+import sys
 import typing
 
 from colorama import Fore
 
-__author__: typing.Final = "Parth Mishra"
+__author__: typing.Final = "halfstackpgr"
 __email__: typing.Final = "halfstackpgr@gmail.com"
 __version__: typing.Final = "0.0.1"
-__description__: typing.Final = (
+__telegram_api__ : typing.Final = "Bot API 7.7"
+__github_url__ : typing.Final = "https://github.com/halfstackpgr/kiran"
+__pypi_url__ : typing.Final = "https://pypi.org/project/kiran"
+__short_description__: typing.Final = (
     "A raged framework for wrapping up your telegram bot."
 )
-__banner__: typing.Final = rf"""{Fore.LIGHTRED_EX} __   ___   __      _______        __      _____  ___   
-|/"| /  ") |" \    /"      \      /""\    (\"   \|"  \  
-(: |/   /  ||  |  |:        |    /    \   |.\\   \    | {Fore.RESET}
-|    __/   {Fore.BLUE}|:  |  |_____/   )   /' /\  \  |:{Fore.RESET} \.   \\  | 
-(// _  \   {Fore.BLUE}|.  |   //      /   //  __'  \ |.{Fore.RESET}  \    \. | 
-{Fore.LIGHTGREEN_EX}|: | \  \  /\  |\ |:  __   \  /   /  \\  \|    \    \ | 
-(__|  \__)(__\_|_)|__|  \___)(___/    \___)\___|\____\){Fore.RESET}
 
-{__description__}
+__banner__ : typing.Final = f"""
+    {Fore.LIGHTRED_EX}oooo    oooo o8o{Fore.RESET}                                    {Fore.CYAN}Author:{Fore.RESET} {__author__}
+    {Fore.LIGHTRED_EX}`888   .8P'  `"'{Fore.RESET}                                    {Fore.CYAN}Email:{Fore.RESET} {__email__}
+    {Fore.WHITE}888  d8'    oooo  oooo d8b  .oooo.   ooo. .oo.{Fore.RESET}      {Fore.CYAN}Python Version:{Fore.RESET} {sys.version[:6]}
+    {Fore.BLUE}88888[      `888  `8888P   `P  )88b  `888P"Y88b{Fore.RESET}     {Fore.CYAN}Wrapper Version:{Fore.RESET} {__version__}
+    {Fore.WHITE}888`88b.     888   888      .oP"888   888   888{Fore.RESET}     {Fore.CYAN}Telegram API:{Fore.RESET} {__telegram_api__}
+    {Fore.LIGHTGREEN_EX}888  `88b.   888   888     d8(  888   888   888{Fore.RESET}     {Fore.CYAN}Github URL:{Fore.RESET} halfstackpgr/kiran
+    {Fore.LIGHTGREEN_EX}o888o  o888o o888o d888b   `Y8888o   o888o o888o{Fore.RESET}    {Fore.MAGENTA}⭐ The Repo if you find it helpful.{Fore.RESET}
 
-Author: {Fore.MAGENTA}{__author__}{Fore.RESET}
-Email: {Fore.MAGENTA}{__email__}{Fore.RESET}
-Version: {Fore.MAGENTA}{__version__}{Fore.RESET}
+                    {__short_description__}
 """
-
-__github_url__ = "https://github.com/halfstackpgr/kiran"
